aa0b83fc2c358c02896340e53e337897ea773daf
Sam Henderson <hendersonsam456@gmail.com>
2019-02-06 19:07:21 -0800

    Finished part 2

--- shopping-challenge/src/App.js
+++ shopping-challenge/src/App.js
@@ -5,13 +5,20 @@ import Navbar from './Navbar.js'
 import Cart from './pages/Cart.js'
 import Home from './pages/Home.js'
 
+const bikes = [
+    {id: 1, name: "City Rider"},
+    {id: 2, name: "France"},
+    {id: 3, name: "Fixie"},
+]
+
+
 class App extends Component {
   render() {
     return (
       <Router>
           <div className="container">
               <Navbar />
-              <Route exact path="/" component={Home} />
+              <Route exact path="/" component={()=><Home bikes={bikes}/>} />
               <Route path="/cart" component={Cart} />
           </div>
       </Router>
--- shopping-challenge/src/pages/Home.js
+++ shopping-challenge/src/pages/Home.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import '../App.css';
-
+import React, { Component } from "react";
+import "../App.css";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home
+  render() {
+    console.log(this.props);
+    return (
+      <div className="bike-list">
+        {this.props.bikes.map(bike => {
+          return (
+            <div className="bike-card" key={bike.id}>
+                <div>ID: {bike.id}</div>
+                <div>Name: {bike.name}</div>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
